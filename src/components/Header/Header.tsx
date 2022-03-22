92e6da72e4b4183897ce27d7af0660c06385a35e
sophiecode1105 <sophiecode1105@gmail.com>
2022-03-22 14:49:39 +0900

    feat: add app header

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,38 @@
+import styled from "styled-components";
+import logo from "../../assets/Hits_logo.png";
+
+const Container = styled.div`
+  width: 100%;
+  height: 100px;
+  position: absolute;
+  left: 0%;
+  right: 0%;
+  top: 0%;
+  bottom: 0%;
+
+  background: #ffffff;
+  filter: drop-shadow(0px 1px 4px rgba(21, 34, 50, 0.25));
+`;
+
+const Logo = styled.div`
+  position: absolute;
+  left: 1.04%;
+  right: 85.07%;
+  top: 14%;
+  bottom: 13%;
+
+  background-size: cover;
+  background-image: url(${logo});
+  background-repeat: no-repeat;
+  background-position: center center;
+`;
+
+const Header = () => {
+  return (
+    <Container>
+      <Logo />
+    </Container>
+  );
+};
+
+export default Header;
---
+++ src/pages/Main.tsx
@@ -0,0 +1,7 @@
+import Header from "../components/Header/Header";
+
+const Main = () => {
+  return <Header />;
+};
+
+export default Main;
