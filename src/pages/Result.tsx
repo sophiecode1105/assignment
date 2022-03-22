e07ec6a06fd6fcb4fe740b6fce78754d6e2451c7
sophiecode1105 <sophiecode1105@gmail.com>
2022-03-22 18:47:52 +0900

    feat: Result 헤더 구현

--- src/pages/Result.tsx
+++ src/pages/Result.tsx
@@ -1,8 +1,38 @@
+import styled from "styled-components";
+import Search from "../components/Result/Search";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 85%;
+  margin: 79px auto 0 auto;
+`;
+
+const Header = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  height: 103px;
+  border-bottom: 1px solid #0d9991;
+`;
+
+const Title = styled.h1`
+  flex: 0.5;
+  height: 100%;
+  margin: 0;
+  font-family: Lato;
+  font-size: 32px;
+  text-align: left;
+`;
+
 const Result = () => {
   return (
-    <>
-      <h1>Result</h1>
-    </>
+    <Container>
+      <Header>
+        <Title>Result</Title>
+        <Search />
+      </Header>
+    </Container>
   );
 };
 
