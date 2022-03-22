267c4bab2a2966262e9a1d360a2e73e58adf7645
sophiecode1105 <sophiecode1105@gmail.com>
2022-03-22 21:58:42 +0900

    refactor: 헤더, 네비게이션 App 컴퍼넌트로 이동

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,24 @@
 import Routers from "./Routers";
+import styled from "styled-components";
+import Header from "./Header/Header";
+import Navbar from "./Navbar/Navbar";
+import { BrowserRouter } from "react-router-dom";
+
+const Container = styled.div`
+  position: relative;
+  width: 100vw;
+`;
 
 const App = () => {
-  return <Routers />;
+  return (
+    <BrowserRouter>
+      <Container>
+        <Header />
+        <Navbar />
+        <Routers />
+      </Container>
+    </BrowserRouter>
+  );
 };
 
 export default App;
