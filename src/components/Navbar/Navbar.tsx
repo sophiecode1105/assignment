897fc1ecba4dcfc482dd8a6c0572e748ed664f39
sophiecode1105 <sophiecode1105@gmail.com>
2022-03-22 16:36:13 +0900

    feat: 네비게이션바 추가 및 라우터에 routes 추가

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,73 @@
+import styled from "styled-components";
+import { useNavigate } from "react-router-dom";
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  width: 95%;
+  height: 78px;
+  margin: 123px auto 0px auto;
+  background-color: #fcfefe;
+  border-radius: 16px;
+  filter: drop-shadow(1px 4px 4px rgba(0, 0, 0, 0.25));
+`;
+
+const TitleWrap = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 60%;
+  height: 40px;
+  margin-left: 30.45px;
+`;
+
+const NavTitle = styled.div`
+  width: 12.25%;
+  text-align: center;
+  font-size: 20px;
+  font-family: Lato;
+  font-weight: 500;
+  cursor: pointer;
+`;
+
+const ProejctNameWrap = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  margin-right: 27px;
+  width: 40%;
+  height: 40px;
+`;
+const ProjectName = styled.div`
+  color: #0d9991;
+  font-weight: 700;
+  text-align: center;
+`;
+
+const Navbar = () => {
+  const navigate = useNavigate();
+  const Titles = ["Alpha", "Bravo", "Charlie", "Delta", "Echo"];
+
+  const pageTurningToFallback = (title: string) => {
+    navigate(`/${title.toLowerCase()}`);
+  };
+
+  const pageTurningToResult = () => {
+    navigate("/result");
+  };
+
+  return (
+    <Container>
+      <TitleWrap>
+        {Titles.map((title, idx) => {
+          return <NavTitle onClick={() => pageTurningToFallback(title)}>{title}</NavTitle>;
+        })}
+        <NavTitle onClick={pageTurningToResult}>Result</NavTitle>
+      </TitleWrap>
+      <ProejctNameWrap>
+        <ProjectName>project_name</ProjectName>
+      </ProejctNameWrap>
+    </Container>
+  );
+};
+export default Navbar;
--- src/components/Routers.tsx
+++ src/components/Routers.tsx
@@ -1,11 +1,35 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Header from "./Header/Header";
+import Navbar from "./Navbar/Navbar";
 import Main from "../pages/Main";
+import FallBack from "../pages/Fallback";
+import Result from "../pages/Result";
 
 const Routers = () => {
   return (
     <BrowserRouter>
+      <Header />
+      <Navbar />
       <Routes>
         <Route path="/" element={<Main />} />
+      </Routes>
+      <Routes>
+        <Route path="/alpha" element={<FallBack />} />
+      </Routes>
+      <Routes>
+        <Route path="/bravo" element={<FallBack />} />
+      </Routes>
+      <Routes>
+        <Route path="/charlie" element={<FallBack />} />
+      </Routes>
+      <Routes>
+        <Route path="/delta" element={<FallBack />} />
+      </Routes>
+      <Routes>
+        <Route path="/echo" element={<FallBack />} />
+      </Routes>
+      <Routes>
+        <Route path="/result" element={<Result />} />
       </Routes>
     </BrowserRouter>
   );
