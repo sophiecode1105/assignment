f7a5846192368c2416d001d99ad6a0713769ce10
sophiecode1105 <sophiecode1105@gmail.com>
2022-03-22 22:54:06 +0900

    refactor: 불필요한 import 삭제 (header/navigation)

--- src/components/Routers.tsx
+++ src/components/Routers.tsx
@@ -1,25 +1,19 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Header from "./Header/Header";
-import Navbar from "./Navbar/Navbar";
+import { Route, Routes } from "react-router-dom";
 import Main from "../pages/Main";
 import FallBack from "../pages/Fallback";
 import Result from "../pages/Result";
 
 const Routers = () => {
   return (
-    <BrowserRouter>
-      <Header />
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/alpha" element={<FallBack />} />
-        <Route path="/bravo" element={<FallBack />} />
-        <Route path="/charlie" element={<FallBack />} />
-        <Route path="/delta" element={<FallBack />} />
-        <Route path="/echo" element={<FallBack />} />
-        <Route path="/result" element={<Result />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Main />} />
+      <Route path="/alpha" element={<FallBack />} />
+      <Route path="/bravo" element={<FallBack />} />
+      <Route path="/charlie" element={<FallBack />} />
+      <Route path="/delta" element={<FallBack />} />
+      <Route path="/echo" element={<FallBack />} />
+      <Route path="/result" element={<Result />} />
+    </Routes>
   );
 };
 
