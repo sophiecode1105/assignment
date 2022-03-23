13b9d215aa16cb925ac957f73dfad2cb17c4501c
sophiecode1105 <sophiecode1105@gmail.com>
2022-03-24 02:23:49 +0900

    refactor: 서브테이블 파일  분기

---
+++ src/components/Result/Table/SubTable/SubTableContent.tsx
@@ -0,0 +1,29 @@
+import { useSelector } from "react-redux";
+import styled from "styled-components";
+import { UserSubDataList } from "../../../../model/user";
+import { RootState } from "../../../../state/store/store";
+import SubTableRow from "./SubTableRow";
+
+const Container = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+`;
+
+interface SubTableContentProps {
+  userName: string;
+}
+
+const SubTableContent = ({ userName }: SubTableContentProps) => {
+  const subList: UserSubDataList = useSelector(
+    (state: RootState) => state.userData.value.filter((userData) => userData.data[0] === userName)[0].subDataList
+  );
+  return (
+    <Container>
+      {subList.map((subData) => (
+        <SubTableRow rowData={subData.data} />
+      ))}
+    </Container>
+  );
+};
+export default SubTableContent;
