2a7c166ba46a35209b4d78e18c8cc5a41c21c4ea
sophiecode1105 <sophiecode1105@gmail.com>
2022-03-24 02:21:34 +0900

    feat: SubTable 폴더  생성및 기능 일부 구현

---
+++ src/components/Result/Table/TableSortButton.tsx
@@ -0,0 +1,48 @@
+import styled from "styled-components";
+import up from "../../../assets/Polygon_Up.png";
+import down from "../../../assets/Polygon_Down.png";
+import { FOX, GOLF, ASC, DESC, SortKey } from "../../../model/constants";
+import { useAppDispatch } from "../../../state/store/hook";
+import { sortUserDataList } from "../../../state/store/userData";
+
+const Container = styled.div`
+  margin-left: 10px;x
+`;
+
+const Button = styled.div`
+  user-select: none;
+  cursor: pointer;
+`;
+
+const ButtonImg = styled.img``;
+
+interface SortButtonProps {
+  sortKey: SortKey;
+}
+
+const TableSortButton = ({ sortKey }: SortButtonProps) => {
+  const dispatch = useAppDispatch();
+
+  return (
+    <Container>
+      <Button>
+        <ButtonImg
+          onClick={() => {
+            dispatch(sortUserDataList({ sort: sortKey, direc: ASC }));
+          }}
+          draggable={false}
+          src={up}
+        ></ButtonImg>
+      </Button>
+      <Button>
+        <ButtonImg
+          onClick={() => dispatch(sortUserDataList({ sort: sortKey, direc: DESC }))}
+          draggable={false}
+          src={down}
+        ></ButtonImg>
+      </Button>
+    </Container>
+  );
+};
+
+export default TableSortButton;
