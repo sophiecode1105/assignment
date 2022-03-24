c0e31653e8a88396a7f0501f80e4fac16c4be04e
sophiecode1105 <sophiecode1105@gmail.com>
2022-03-24 20:22:02 +0900

    feat: X 버튼 선택해제 기능 구현

--- src/components/Result/Print/Print.tsx
+++ src/components/Result/Print/Print.tsx
@@ -1,25 +1,52 @@
 import { useSelector } from "react-redux";
 import styled from "styled-components";
 import { RootState } from "../../../state/store/store";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faXmark } from "@fortawesome/free-solid-svg-icons";
+import { useAppDispatch } from "../../../state/store/hook";
+import { changeSubItemCheck } from "../../../state/store/userData";
 
 const Container = styled.div`
   width: 100%;
+  margin-top: 1%;
 `;
 
-const PrintNameWithIdWrap = styled.div`
+const ItemContainer = styled.div`
   background-color: #9ed6d3;
 `;
-const PrintNameWithId = styled.div`
+const ItemKey = styled.div`
+  display: flex;
   font-size: 24px;
   font-weight: 700;
-  margin-top: 1%;
   padding: 1%;
+  min-width: 100px;
+  align-items: center;
+  justify-content: flex-start;
+`;
+
+const Item = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+`;
+
+const IconWrap = styled.div`
+  display: flex;
+  margin: 0 5%;
+  font-size: 1.5em;
+  cursor: pointer;
+  &:hover {
+    color: red;
+  }
 `;
 
 const Print = () => {
-  const checkedDataByName: { name: string; clicked: number[] }[] = useSelector((state: RootState) =>
+  const dispatch = useAppDispatch();
+
+  const checkedDataByName: { userId: string; name: string; clicked: number[] }[] = useSelector((state: RootState) =>
     state.userData.value.map((userData) => {
       return {
+        userId: userData.id,
         name: userData.data[0],
         clicked: userData.clicked,
       };
@@ -29,11 +56,20 @@ const Print = () => {
     <Container>
       {checkedDataByName.map((el, i) => {
         return (
-          <PrintNameWithIdWrap key={`p-${i}`}>
+          <ItemContainer key={`p-${i}`}>
             {el.clicked.map((il, j) => (
-              <PrintNameWithId key={`p-i-${i}-${j}`}>{`${el.name}-${il}`}</PrintNameWithId>
+              <Item>
+                <ItemKey key={`p-i-${i}-${j}`}>{`${el.name}-${il}`}</ItemKey>
+                <IconWrap
+                  onClick={() => {
+                    dispatch(changeSubItemCheck({ userId: el.userId, id: il }));
+                  }}
+                >
+                  <FontAwesomeIcon icon={faXmark} />
+                </IconWrap>
+              </Item>
             ))}
-          </PrintNameWithIdWrap>
+          </ItemContainer>
         );
       })}
     </Container>
